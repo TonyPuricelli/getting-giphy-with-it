e38718406c330933d60c39f0c9789ef61e8cfc13
Tony Puricelli <tony_puricelli@Tonys-MacBook-Pro.local>
2018-03-27 11:50:21 -0500

    adds JS to display GIFs and animate or pause on user click

--- assets/giphy.js
+++ assets/giphy.js
@@ -11,14 +11,14 @@ renderButtons();
 
 //trigger AJAX call on button clicks
 $(".showBtn").on("click", function() {
-    showMeGIFs();
+    showMeGIFs($(this).attr("data-name"));
 });
 
 // add button on user submission
 $("#submit").on("click", function(event) {
     event.preventDefault();
 
-    if ($("#submitBox")) {
+    if ($("#submitBox").val()) {
     // variable to store show from button
     var show = $("#submitBox").val().trim();
     console.log(show);
@@ -40,7 +40,7 @@ function renderButtons() {
     for (var i = 0; i < listOfShows.length; i++) {
         // create new buttons with common class and unique data type
         var newBtn = $("<button>");
-        newBtn.addClass("showBtn");
+        newBtn.addClass("showBtn btn btn-secondary");
         newBtn.attr("data-name", listOfShows[i]);
         newBtn.text(listOfShows[i]);
         // appends to buttons div
@@ -61,7 +61,57 @@ function showMeGIFs(show) {
         method: 'GET'
       }).then(function(response) {
         console.log(response);
-        // add code here
+
+        // store results of ajax call
+        var results = response.data;
+
+        // loop through each item
+        for (var i = 0; i < results.length; i++) {
+            // creating image tag
+            var newImage = $("<img>");
+
+            // create variables for still and animated images
+            var loopingImg = results[i].images.fixed_height.url;
+            var stillImg = results[i].images.fixed_height_still.url;
+            console.log(loopingImg);
+            console.log(stillImg);
+
+            // add still image as source and as an alt attribute
+            newImage.attr("src", stillImg);
+            newImage.attr("data-still", stillImg);
+
+            // add video as alt for swap later
+            newImage.attr("data-animate", loopingImg);
+
+            // set data state for image
+            newImage.attr("data-state", "still");
+
+            // add common class to image tags
+            newImage.addClass("gif");
+
+            // Creating a paragraph tag with the result item's rating
+            var p = $("<p>").text("Rating: " + results[i].rating);
+
+            // append rating to image
+            newImage.append(p);
+
+            // append img tag to display div
+            $("#display").append(newImage);
+        
+        };  
+      });
+
+      // click event to pause or animate images
+      $(document).on("click", ".gif", function(event) {
+        var state = $(this).attr("data-state");
+
+        if (state === "still") {
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+          } else {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+          }
       });
 };
 
